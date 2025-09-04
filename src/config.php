b7a8c1b6ee01d215efe799d1a1153d24672d8891
ishailendra26 <ishailendra26@gmail.com>
2025-09-04 14:00:15 +0530

    added git ignore

--- src/config.php
+++ src/config.php
@@ -1,13 +1,13 @@
 <?php
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u163236363_ez');
-define('DB_USER', 'u163236363_ez');
-define('DB_PASS', 'Shailu26ap@');
+define('DB_NAME', 'sarkari');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 // Site Configuration
 define('SITE_NAME', 'Examsz - Your Exam Success Partner | Jobs, Results, Admit Cards & Syllabus | Mock Tests');
-define('SITE_URL', 'https://examsz.in');
+define('SITE_URL', 'http://localhost/sarkari/');
 define('SITE_DESCRIPTION', 'Latest Government Jobs, Results, Admit Cards & Syllabus | Mock Tests');
 
 // Community / Social Channels
